437d75fab05a07f65329853379be55c0b113da5c
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2016-09-09 13:58:48 +0200

    changelog, better mkdirs() backporting 3.2 exist_ok on PY2

--- graphviz/_compat.py
+++ graphviz/_compat.py
@@ -1,5 +1,6 @@
 # _compat.py - Python 2/3 compatibility
 
+import os
 import sys
 
 PY2 = sys.version_info[0] == 2
@@ -11,9 +12,18 @@ if PY2:  # pragma: no cover
     def iteritems(d):
         return d.iteritems()
 
+    def makedirs(name, mode=0o777, exist_ok=False):
+        try:
+            os.makedirs(name, mode)
+        except OSError:
+            if not exist_ok or not os.path.isdir(name):
+                raise
+
 
 else:  # pragma: no cover
     text_type = str
 
     def iteritems(d):
         return iter(d.items())
+
+    makedirs = os.makedirs
--- graphviz/tools.py
+++ graphviz/tools.py
@@ -1,9 +1,8 @@
 # tools.py
 
 import os
-import errno
 
-from ._compat import iteritems
+from . import _compat
 
 __all__ = ['attach', 'mkdirs', 'mapping_items']
 
@@ -18,20 +17,13 @@ def attach(object, name):
 
 def mkdirs(filename, mode=0o777):
     """Recursively create directories up to the path of filename as needed."""
-    path = os.path.dirname(filename)
-    if not path:
+    dirname = os.path.dirname(filename)
+    if not dirname:
         return
-
-    try:
-        os.makedirs(path, mode)
-    except OSError as e:
-        if e.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
+    _compat.makedirs(dirname, mode=mode, exist_ok=True)
 
 
-def mapping_items(mapping):
+def mapping_items(mapping, _iteritems=_compat.iteritems):
     """Return an iterator over the mapping items, sort if it's a plain dict.
 
     >>> list(mapping_items({'spam': 0, 'ham': 1, 'eggs': 2}))
@@ -42,5 +34,5 @@ def mapping_items(mapping):
     [(0, 'spam'), (1, 'ham'), (2, 'eggs')]
     """
     if type(mapping) is dict:
-        return iter(sorted(iteritems(mapping)))
-    return iteritems(mapping)
+        return iter(sorted(_iteritems(mapping)))
+    return _iteritems(mapping)
