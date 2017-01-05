5fa23369b508472ee1b552fe87cc24c79137153b
Diane Trout <diane@ghic.org>
2017-01-05 14:25:42 -0800

    Ensure test precondition.
    As written the tests in test_tools assume the tests will be run in the
    root of the source and that a file named setup.py will exist.
    
    The Debian test runner installs the package into a subdirectory,
    and then runs the tests there.
    
    This adds a test setUp and tearDown to add (and remove) an empty
    setup.py if no setup.py file already exists

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,11 +2,21 @@
 
 import unittest2 as unittest
 import os
+import stat
 
 from graphviz.tools import mkdirs
 
 
 class TestMkdirs(unittest.TestCase):
+    def setUp(self):
+        if not os.path.exists('setup.py'):
+            f = open('setup.py', 'w')
+            f.close()
+
+    def tearDown(self):
+        info = os.stat('setup.py')
+        if info.st_size == 0:
+            os.unlink('setup.py')
 
     @staticmethod
     def _dirnames(path=os.curdir):
