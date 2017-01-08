a2eb0d933c278e74c7d7601ea9b490f171fee2eb
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-01-08 13:54:12 +0100

    run more extensive mkdirs tests inside tempfile.mkdtemp()

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,21 +2,47 @@
 
 import unittest2 as unittest
 import os
+import shutil
+import tempfile
+import functools
 
 from graphviz.tools import mkdirs
 
 
+def itertree(root):
+    for path, dirs, files in os.walk(root):
+        base = os.path.relpath(path, root)
+        rel_path = functools.partial(os.path.join, base if base != '.' else '')
+        for is_file, names in [(False, dirs), (True, files)]:
+            for n in names:
+                yield is_file, rel_path(n).replace('\\', '/')
+
+
 class TestMkdirs(unittest.TestCase):
 
-    @staticmethod
-    def _dirnames(path=os.curdir):
-        return [name for name in os.listdir(path) if os.path.isdir(name)]
+    def setUp(self):
+        self.old_dir = os.getcwd()
+        self.test_dir = tempfile.mkdtemp()
+        assert not self._dentries()
+        os.chdir(self.test_dir)
 
-    def test_cwd(self):
-        dirnames = self._dirnames()
-        mkdirs('setup.py')
-        self.assertEqual(self._dirnames(), dirnames)
+    def tearDown(self):
+        os.chdir(self.old_dir)
+        shutil.rmtree(self.test_dir)
 
-    def test_file(self):
+    def _dentries(self):
+        return list(itertree(self.test_dir))
+
+    def test_mkdirs(self):
+        mkdirs('spam.eggs')
+        self.assertEqual(self._dentries(), [])
+        for _ in range(2):
+            mkdirs('spam/eggs/spam.eggs')
+            self.assertEqual(self._dentries(),
+                             [(False, 'spam'), (False, 'spam/eggs')])
+
+    def test_mkdirs_invalid(self):
+        with open('spam.eggs', 'wb'):
+            pass
         with self.assertRaises(OSError):
-            mkdirs('setup.py/spam')
+            mkdirs('spam.eggs/spam')
