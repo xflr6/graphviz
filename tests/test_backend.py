609631ab73f66191a7e7d618aa8c6ab74b67eb05
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-04-29 09:19:05 +0200

    test engine and format in render() and pipe()

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -8,13 +8,29 @@ from graphviz.backend import render, pipe
 
 class TestRender(unittest.TestCase):
 
-    def test_render_missing_file(self):
+    def test_render_engine_unknown(self):
+        with self.assertRaisesRegexp(ValueError, r'engine'):
+            render('spam', 'pdf', '')
+
+    def test_render_format_unknown(self):
+        with self.assertRaisesRegexp(ValueError, r'format'):
+            render('dot', 'spam', '')
+
+    def test_render_filepath_missing(self):
         with self.assertRaises(subprocess.CalledProcessError) as c:
             render('dot', 'pdf', 'doesnotexist')
         self.assertEqual(c.exception.returncode, 2)
 
 
 class TestPipe(unittest.TestCase):
+
+    def test_render_engine_unknown(self):
+        with self.assertRaisesRegexp(ValueError, r'engine'):
+            pipe('spam', 'pdf', b'')
+
+    def test_render_format_unknown(self):
+        with self.assertRaisesRegexp(ValueError, r'format'):
+            pipe('dot', 'spam', b'')
 
     def test_pipe_invalid_dot(self):
         with self.assertRaises(subprocess.CalledProcessError) as c:
