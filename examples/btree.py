da2bbefe2b19c933894941e7304377e0a75eb13f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-12-29 16:42:01 +0100

    replace trailing whitespace workaround with nohtml()

--- examples/btree.py
+++ examples/btree.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # btree.py - http://www.graphviz.org/pdf/dotguide.pdf Figure 13
 
-from graphviz import Digraph
+from graphviz import Digraph, nohtml
 
 g = Digraph('g', filename='btree.gv', node_attr={'shape': 'record', 'height': '.1'})
 
-g.node('node0', '<f0> |<f1> G|<f2> ')
-g.node('node1', '<f0> |<f1> E|<f2> ')
-g.node('node2', '<f0> |<f1> B|<f2> ')
-g.node('node3', '<f0> |<f1> F|<f2> ')
-g.node('node4', '<f0> |<f1> R|<f2> ')
-g.node('node5', '<f0> |<f1> H|<f2> ')
-g.node('node6', '<f0> |<f1> Y|<f2> ')
-g.node('node7', '<f0> |<f1> A|<f2> ')
-g.node('node8', '<f0> |<f1> C|<f2> ')
+g.node('node0', nohtml('<f0> |<f1> G|<f2>'))
+g.node('node1', nohtml('<f0> |<f1> E|<f2>'))
+g.node('node2', nohtml('<f0> |<f1> B|<f2>'))
+g.node('node3', nohtml('<f0> |<f1> F|<f2>'))
+g.node('node4', nohtml('<f0> |<f1> R|<f2>'))
+g.node('node5', nohtml('<f0> |<f1> H|<f2>'))
+g.node('node6', nohtml('<f0> |<f1> Y|<f2>'))
+g.node('node7', nohtml('<f0> |<f1> A|<f2>'))
+g.node('node8', nohtml('<f0> |<f1> C|<f2>'))
 
 g.edge('node0:f2', 'node4:f1')
 g.edge('node0:f0', 'node1:f1')
