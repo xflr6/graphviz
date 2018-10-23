eab3153f80164e14672e1b25e988611845922917
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-10-23 09:42:55 +0200

    release 0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphviz',
-    version='0.10.dev0',
+    version='0.10',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Simple Python interface for Graphviz',
