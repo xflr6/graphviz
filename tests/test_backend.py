ae5f1a735f6fe73084f62015ac2812ae8a30d364
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-11-02 19:13:07 +0100

    flake8

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -101,7 +101,8 @@ def test_pipe_missing_executable():
 
 
 @pytest.exe
-@pytest.mark.xfail('version() == (2, 36, 0)', reason='https://bugs.launchpad.net/ubuntu/+source/graphviz/+bug/1694108')
+@pytest.mark.xfail('version() == (2, 36, 0)',
+                   reason='https://bugs.launchpad.net/ubuntu/+source/graphviz/+bug/1694108')
 def test_pipe_invalid_data(capsys, quiet, engine='dot', format_='svg'):
     with pytest.raises(subprocess.CalledProcessError) as e:
         pipe(engine, format_, b'nongraph', quiet=quiet)
