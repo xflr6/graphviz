b52ecd2b70d5d82f6e9cdd00e4c8e6aab4d45cef
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-11-11 22:07:18 +0100

    drop thonny workaround

--- run-tests.py
+++ run-tests.py
@@ -10,7 +10,7 @@ ARGS = [
     #'--pdb',
 ]
 
-if 'idlelib' in sys.modules or 'thonny' in sys.modules:
+if 'idlelib' in sys.modules:
     ARGS.extend(['--capture=sys', '--color=no'])
 elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
     ARGS.append('--capture=sys')
