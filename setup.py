cc1f224e615ab079aab88b00075cf1ac4c5c130c
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-12-27 11:43:33 +0100

    fix failing ci requiring mock 3 features

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     platforms='any',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     extras_require={
-        'dev': ['tox>=3.0', 'flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['mock>=2', 'pytest>=3.4,!=3.10.0', 'pytest-mock>=1.8', 'pytest-cov'],
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['mock>=3', 'pytest>=3.4,!=3.10.0', 'pytest-mock>=1.8', 'pytest-cov'],
         'docs': ['sphinx>=1.7', 'sphinx-rtd-theme'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
