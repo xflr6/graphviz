c98c5eea2e81237d2e99d4785fbcd9d984e17cfd
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-01-28 18:21:02 +0100

    add project_urls for PyPI links

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     keywords='graph visualization dot render',
     license='MIT',
     url='https://github.com/xflr6/graphviz',
+    project_urls={
+        'Documentation': 'https://graphviz.readthedocs.io',
+        'Changelog': 'https://graphviz.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/xflr6/graphviz/issues',
+    },
     packages=find_packages(),
     platforms='any',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
