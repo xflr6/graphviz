6d3b9e7507b12a61c24b03b8e79a8695f381a237
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-04-13 21:21:34 +0200

    use run-tests.py as single entry point for testing

--- run-tests.py
+++ run-tests.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
-# run-tests.py
 
+import platform
 import sys
 
 import pytest
 
 ARGS = [
-    #'--exitfirst',
     #'--pdb',
+    #'--exitfirst',
 ]
 
-if 'idlelib' in sys.modules:
-    ARGS.extend(['--capture=sys', '--color=no'])
-elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
-    ARGS.append('--capture=sys')
+if platform.system() == 'Windows':
+    if 'idlelib' in sys.modules:
+        ARGS.extend(['--capture=sys', '--color=no'])
+    elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
+        ARGS.append('--capture=sys')
 
-pytest.main(ARGS + sys.argv[1:])
+sys.exit(pytest.main(ARGS + sys.argv[1:]))
