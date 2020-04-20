997705c41b5c1a7d8940a4d6d8c76dacf558e174
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-04-20 13:56:18 +0200

    add sphinx.ext.viewcode extension to include source links in docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
