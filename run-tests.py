661abc7d7c498bd5f8018809bf89d4ed9c144218
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-01-05 21:19:35 +0100

    drop Python 2 from test environments

--- run-tests.py
+++ run-tests.py
@@ -13,7 +13,5 @@ ARGS = [
 if platform.system() == 'Windows':
     if 'idlelib' in sys.modules:
         ARGS.extend(['--capture=sys', '--color=no'])
-    elif sys.version_info.major == 2 and 'win_unicode_console' in sys.modules:
-        ARGS.append('--capture=sys')
 
 sys.exit(pytest.main(ARGS + sys.argv[1:]))
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     packages=find_packages(),
     platforms='any',
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
+    python_requires='>=3.6',
     extras_require={
         'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
         'test': ['mock>=3', 'pytest>=4', 'pytest-mock>=2', 'pytest-cov'],
