xflr6/graphviz

commit 4ebb566effe34719fd0e3284ea54a300e5e7b536

prefer f-strings over percent-interpolation for simple cases

examples/traffic_lights.py
@@ -7,26 +7,26 @@ t = Digraph('TrafficLights', filename='traffic_lights.gv', engine='neato')
 
 t.attr('node', shape='box')
 for i in (2, 1):
-    t.node('gy%d' % i)
-    t.node('yr%d' % i)
-    t.node('rg%d' % i)
+    t.node(f'gy{i:d}')
+    t.node(f'yr{i:d}')
+    t.node(f'rg{i:d}')
 
 t.attr('node', shape='circle', fixedsize='true', width='0.9')
 for i in (2, 1):
-    t.node('green%d' % i)
-    t.node('yellow%d' % i)
-    t.node('red%d' % i)
-    t.node('safe%d' % i)
+    t.node(f'green{i:d}')
+    t.node(f'yellow{i:d}')
+    t.node(f'red{i:d}')
+    t.node(f'safe{i:d}')
 
 for i, j in [(2, 1), (1, 2)]:
-    t.edge('gy%d' % i, 'yellow%d' % i)
-    t.edge('rg%d' % i, 'green%d' % i)
-    t.edge('yr%d' % i, 'safe%d' % j)
-    t.edge('yr%d' % i, 'red%d' % i)
-    t.edge('safe%d' % i, 'rg%d' % i)
-    t.edge('green%d' % i, 'gy%d' % i)
-    t.edge('yellow%d' % i, 'yr%d' % i)
-    t.edge('red%d' % i, 'rg%d' % i)
+    t.edge(f'gy{i:d}', f'yellow{i:d}')
+    t.edge(f'rg{i:d}', f'green{i:d}')
+    t.edge(f'yr{i:d}', f'safe{i:d}')
+    t.edge(f'yr{i:d}', f'red{i:d}')
+    t.edge(f'safe{i:d}', f'rg{i:d}')
+    t.edge(f'green{i:d}', f'gy{i:d}')
+    t.edge(f'yellow{i:d}', f'yr{i:d}')
+    t.edge(f'red{i:d}', f'rg{i:d}')
 
 t.attr(overlap='false')
 t.attr(label=r'PetriNet Model TrafficLights\n'

examples/angles.py
@@ -12,8 +12,8 @@ with g.subgraph(name='cluster_1') as c:
            gradientangle='360', label='n9:360', fontcolor='black')
     c.node('n9')
     for i, a in zip(range(8, 0, -1), range(360 - 45, -1, -45)):
-        c.attr('node', gradientangle='%d' % a, label='n%d:%d' % (i, a))
-        c.node('n%d' % i)
+        c.attr('node', gradientangle=f'{a:d}', label=f'n{i:d}:{a:d}')
+        c.node(f'n{i:d}')
     c.attr(label='Linear Angle Variations (white to black gradient)')
 
 with g.subgraph(name='cluster_2') as c:
@@ -22,8 +22,8 @@ with g.subgraph(name='cluster_2') as c:
            gradientangle='360', label='n18:360', fontcolor='black')
     c.node('n18')
     for i, a in zip(range(17, 9, -1), range(360 - 45, -1, -45)):
-        c.attr('node', gradientangle='%d' % a, label='n%d:%d' % (i, a))
-        c.node('n%d' % i)
+        c.attr('node', gradientangle=f'{a:d}', label=f'n{i:d}:{a:d}')
+        c.node(f'n{i:d}')
     c.attr(label='Radial Angle Variations (white to black gradient)')
 
 g.edge('n5', 'n14')

graphviz/backend.py
@@ -88,11 +88,11 @@ log = logging.getLogger(__name__)
 class ExecutableNotFound(RuntimeError):
     """Exception raised if the Graphviz executable is not found."""
 
-    _msg = ('failed to execute %r, '
+    _msg = ('failed to execute {!r}, '
             'make sure the Graphviz executables are on your systems\' PATH')
 
     def __init__(self, args):
-        super().__init__(self._msg % args)
+        super().__init__(self._msg.format(*args))
 
 
 class RequiredArgumentError(Exception):
@@ -103,7 +103,7 @@ class CalledProcessError(subprocess.CalledProcessError):
 
     def __str__(self):
         s = super().__str__()
-        return '%s [stderr: %r]' % (s, self.stderr)
+        return f'{s} [stderr: {self.stderr!r}]'
 
 
 def command(engine, format_, filepath=None, renderer=None, formatter=None):
@@ -112,13 +112,13 @@ def command(engine, format_, filepath=None, renderer=None, formatter=None):
         raise RequiredArgumentError('formatter given without renderer')
 
     if engine not in ENGINES:
-        raise ValueError('unknown engine: %r' % engine)
+        raise ValueError(f'unknown engine: {engine!r}')
     if format_ not in FORMATS:
-        raise ValueError('unknown format: %r' % format_)
+        raise ValueError(f'unknown format: {format_!r}')
     if renderer is not None and renderer not in RENDERERS:
-        raise ValueError('unknown renderer: %r' % renderer)
+        raise ValueError(f'unknown renderer: {renderer!r}')
     if formatter is not None and formatter not in FORMATTERS:
-        raise ValueError('unknown formatter: %r' % formatter)
+        raise ValueError(f'unknown formatter: {formatter!r}')
 
     output_format = [f for f in (format_, renderer, formatter) if f is not None]
     cmd = ['dot', '-K%s' % engine, '-T%s' % ':'.join(output_format)]
@@ -128,7 +128,7 @@ def command(engine, format_, filepath=None, renderer=None, formatter=None):
     else:
         cmd.extend(['-O', filepath])
         suffix = '.'.join(reversed(output_format))
-        rendered = '%s.%s' % (filepath, suffix)
+        rendered = f'{filepath}.{suffix}'
 
     return cmd, rendered
 
@@ -322,7 +322,7 @@ def version():
                    r')?'
                    r' ', out)
     if ma is None:
-        raise RuntimeError('cannot parse %r output: %r' % (cmd, out))
+        raise RuntimeError(f'cannot parse {cmd!r} output: {out!r}')
 
     return tuple(int(d) for d in ma.groups() if d is not None)
 
@@ -345,7 +345,7 @@ def view(filepath, quiet=False):
     try:
         view_func = getattr(view, PLATFORM)
     except AttributeError:
-        raise RuntimeError('platform %r not supported' % PLATFORM)
+        raise RuntimeError(f'platform {PLATFORM!r} not supported')
     view_func(filepath, quiet)
 
 

graphviz/dot.py
@@ -105,7 +105,7 @@ class Dot(files.File):
         yield head % (self._quote(self.name) + ' ' if self.name else '')
 
         for kw in ('graph', 'node', 'edge'):
-            attrs = getattr(self, '%s_attr' % kw)
+            attrs = getattr(self, f'{kw}_attr')
             if attrs:
                 yield self._attr % (kw, self._attr_list(None, attrs))
 
@@ -182,8 +182,8 @@ class Dot(files.File):
         See the :ref:`usage examples in the User Guide <attributes>`.
         """
         if kw is not None and kw.lower() not in ('graph', 'node', 'edge'):
-            raise ValueError('attr statement must target graph, node, or edge: '
-                '%r' % kw)
+            raise ValueError('attr statement must target graph, node, or edge:'
+                             f' {kw!r}')
         if attrs or _attributes:
             if kw is None:
                 a_list = self._a_list(None, attrs, _attributes)
@@ -238,8 +238,8 @@ class Dot(files.File):
             raise ValueError('graph must be sole argument of subgraph()')
 
         if graph.directed != self.directed:
-            raise ValueError('%r cannot add subgraph of different kind:'
-                             ' %r' % (self, graph))
+            raise ValueError(f'{self!r} cannot add subgraph of different kind:'
+                             f' {graph!r}')
 
         lines = ['\t' + line for line in graph.__iter__(subgraph=True)]
         self.body.extend(lines)

graphviz/files.py
@@ -33,7 +33,7 @@ class Base(object):
     def engine(self, engine):
         engine = engine.lower()
         if engine not in backend.ENGINES:
-            raise ValueError('unknown engine: %r' % engine)
+            raise ValueError(f'unknown engine: {engine!r}')
         self._engine = engine
 
     @property
@@ -45,7 +45,7 @@ class Base(object):
     def format(self, format):
         format = format.lower()
         if format not in backend.FORMATS:
-            raise ValueError('unknown format: %r' % format)
+            raise ValueError(f'unknown format: {format!r}')
         self._format = format
 
     @property
@@ -85,7 +85,7 @@ class File(Base):
                  format=None, engine=None, encoding=backend.ENCODING):
         if filename is None:
             name = getattr(self, 'name', None) or self.__class__.__name__
-            filename = '%s.%s' % (name, self._default_extension)
+            filename = f'{name}.{self._default_extension}'
         self.filename = filename
 
         if directory is not None:
@@ -281,17 +281,17 @@ class File(Base):
     def _view(self, filepath, format, quiet):
         """Start the right viewer based on file format and platform."""
         methodnames = [
-            '_view_%s_%s' % (format, backend.PLATFORM),
-            '_view_%s' % backend.PLATFORM,
+            f'_view_{format}_{backend.PLATFORM}',
+            f'_view_{backend.PLATFORM}',
         ]
         for name in methodnames:
             view_method = getattr(self, name, None)
             if view_method is not None:
                 break
         else:
-            raise RuntimeError('%r has no built-in viewer support for %r'
-                               ' on %r platform' % (self.__class__, format,
-                                                    backend.PLATFORM))
+            raise RuntimeError(f'{self.__class__!r} has no built-in viewer'
+                               f' support for {format!r}'
+                               f' on {backend.PLATFORM!r} platform')
         view_method(filepath, quiet)
 
     _view_darwin = staticmethod(backend.view.darwin)

graphviz/lang.py
@@ -69,7 +69,7 @@ def quote(identifier,
     if is_html_string(identifier) and not isinstance(identifier, NoHtml):
         pass
     elif not is_valid_id(identifier) or identifier.lower() in dot_keywords:
-        return '"%s"' % escape_unescaped_quotes(identifier)
+        return f'"{escape_unescaped_quotes(identifier)}"'
     return identifier
 
 
@@ -101,15 +101,15 @@ def a_list(label=None, kwargs=None, attributes=None):
     >>> a_list('spam', {'spam': None, 'ham': 'ham ham', 'eggs': ''})
     'label=spam eggs="" ham="ham ham"'
     """
-    result = ['label=%s' % quote(label)] if label is not None else []
+    result = [f'label={quote(label)}'] if label is not None else []
     if kwargs:
-        items = ['%s=%s' % (quote(k), quote(v))
+        items = [f'{quote(k)}={quote(v)}'
                  for k, v in tools.mapping_items(kwargs) if v is not None]
         result.extend(items)
     if attributes:
         if hasattr(attributes, 'items'):
             attributes = tools.mapping_items(attributes)
-        items = ['%s=%s' % (quote(k), quote(v))
+        items = [f'{quote(k)}={quote(v)}'
                  for k, v in attributes if v is not None]
         result.extend(items)
     return ' '.join(result)
@@ -133,7 +133,7 @@ def attr_list(label=None, kwargs=None, attributes=None):
     content = a_list(label, kwargs, attributes)
     if not content:
         return ''
-    return ' [%s]' % content
+    return f' [{content}]'
 
 
 def escape(s):

tests/conftest.py
@@ -28,7 +28,7 @@ def platform():
 
 
 @pytest.fixture(params=['darwin', 'freebsd', 'linux', 'windows'],
-                ids=lambda p: 'platform=%r' % p)
+                ids=lambda p: f'platform={p!r}')
 def mock_platform(monkeypatch, request):
     monkeypatch.setattr('graphviz.backend.PLATFORM', request.param)
     yield request.param
@@ -59,7 +59,7 @@ def empty_path(monkeypatch):
     monkeypatch.setenv('PATH', '')
 
 
-@pytest.fixture(params=[False, True], ids=lambda q: 'quiet=%r' % q)
+@pytest.fixture(params=[False, True], ids=lambda q: f'quiet={q!r}')
 def quiet(request):
     return request.param
 

tests/test_backend.py
@@ -108,7 +108,7 @@ def test_render(capsys, tmpdir, engine, format_, renderer, formatter,
                 data=b'digraph { hello -> world }'):
     lpath = tmpdir / filename
     lpath.write_binary(data)
-    rendered = lpath.new(ext='%s.%s' % (lpath.ext, expected_suffix))
+    rendered = lpath.new(ext=f'{lpath.ext}.{expected_suffix}')
 
     assert render(engine, format_, str(lpath), renderer, formatter) == str(rendered)
 
@@ -126,10 +126,10 @@ def test_render_img(capsys, tmpdir, filesdir, engine='dot', format_='pdf'):
     assert img_path.size()
 
     gv_path = subdir / 'img.gv'
-    rendered = gv_path.new(ext='%s.%s' % (gv_path.ext, format_))
+    rendered = gv_path.new(ext=f'{gv_path.ext}.{format_}')
     gv_rel, rendered_rel = map(tmpdir.bestrelpath, (gv_path, rendered))
     assert all(s.startswith('subdir') for s in (gv_rel, rendered_rel))
-    gv_path.write_text('graph { red_dot [image="%s"] }' % img_path.basename,
+    gv_path.write_text(f'graph {{ red_dot [image="{img_path.basename}"] }}',
                        encoding='ascii')
 
     with tmpdir.as_cwd():

commit f0c5981bccedf4493eab821ebf98abdefff7a196

fix examples/traffic_lights.py broken in 4ebb566effe34719fd0e3284ea54a300e5e7b536

examples/traffic_lights.py
@@ -21,7 +21,7 @@ for i in (2, 1):
 for i, j in [(2, 1), (1, 2)]:
     t.edge(f'gy{i:d}', f'yellow{i:d}')
     t.edge(f'rg{i:d}', f'green{i:d}')
-    t.edge(f'yr{i:d}', f'safe{i:d}')
+    t.edge(f'yr{i:d}', f'safe{j:d}')
     t.edge(f'yr{i:d}', f'red{i:d}')
     t.edge(f'safe{i:d}', f'rg{i:d}')
     t.edge(f'green{i:d}', f'gy{i:d}')