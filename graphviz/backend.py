4fd9c635dec8d315277dbdc32410bb5322a9263b
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-10-24 15:35:53 +0200

    move view() after run()

--- graphviz/backend.py
+++ graphviz/backend.py
@@ -318,60 +318,6 @@ def version() -> typing.Tuple[int, ...]:
     return tuple(int(d) for d in ma.groups() if d is not None)
 
 
-def view(filepath, quiet: bool = False) -> None:
-    """Open filepath with its default viewing application
-        (platform-specific).
-
-    Args:
-        filepath: Path to the file to open in viewer.
-        quiet: Suppress ``stderr`` output
-            from the viewer process (ineffective on Windows).
-
-    Returns:
-        ``None``
-
-    Raises:
-        RuntimeError: If the current platform is not supported.
-
-    Note:
-        There is no option to wait for the application to close,
-        and no way to retrieve the application's exit status.
-    """
-    try:
-        view_func = getattr(view, PLATFORM)
-    except AttributeError:
-        raise RuntimeError(f'platform {PLATFORM!r} not supported')
-    view_func(filepath, quiet=quiet)
-
-
-@tools.attach(view, 'darwin')
-def view_darwin(filepath, *, quiet: bool) -> None:
-    """Open filepath with its default application (mac)."""
-    cmd = ['open', filepath]
-    log.debug('view: %r', cmd)
-    kwargs = {'stderr': subprocess.DEVNULL} if quiet else {}
-    subprocess.Popen(cmd, **kwargs)
-
-
-@tools.attach(view, 'linux')
-@tools.attach(view, 'freebsd')
-def view_unixoid(filepath, *, quiet: bool) -> None:
-    """Open filepath in the user's preferred application (linux, freebsd)."""
-    cmd = ['xdg-open', filepath]
-    log.debug('view: %r', cmd)
-    kwargs = {'stderr': subprocess.DEVNULL} if quiet else {}
-    subprocess.Popen(cmd, **kwargs)
-
-
-@tools.attach(view, 'windows')
-def view_windows(filepath, *, quiet: bool) -> None:
-    """Start filepath with its associated application (windows)."""
-    # TODO: implement quiet=True
-    filepath = os.path.normpath(filepath)
-    log.debug('view: %r', filepath)
-    os.startfile(filepath)  # pytype: disable=module-attr
-
-
 def run(cmd: typing.Sequence[typing.Union[pathlib.Path, str]],
         *, capture_output: bool = False,
         quiet: bool = False, **kwargs) -> subprocess.CompletedProcess:
@@ -459,3 +405,57 @@ class CalledProcessError(subprocess.CalledProcessError):
     def __str__(self) -> 'str':
         s = super().__str__()
         return f'{s} [stderr: {self.stderr!r}]'
+
+
+def view(filepath, quiet: bool = False) -> None:
+    """Open filepath with its default viewing application
+        (platform-specific).
+
+    Args:
+        filepath: Path to the file to open in viewer.
+        quiet: Suppress ``stderr`` output
+            from the viewer process (ineffective on Windows).
+
+    Returns:
+        ``None``
+
+    Raises:
+        RuntimeError: If the current platform is not supported.
+
+    Note:
+        There is no option to wait for the application to close,
+        and no way to retrieve the application's exit status.
+    """
+    try:
+        view_func = getattr(view, PLATFORM)
+    except AttributeError:
+        raise RuntimeError(f'platform {PLATFORM!r} not supported')
+    view_func(filepath, quiet=quiet)
+
+
+@tools.attach(view, 'darwin')
+def view_darwin(filepath, *, quiet: bool) -> None:
+    """Open filepath with its default application (mac)."""
+    cmd = ['open', filepath]
+    log.debug('view: %r', cmd)
+    kwargs = {'stderr': subprocess.DEVNULL} if quiet else {}
+    subprocess.Popen(cmd, **kwargs)
+
+
+@tools.attach(view, 'linux')
+@tools.attach(view, 'freebsd')
+def view_unixoid(filepath, *, quiet: bool) -> None:
+    """Open filepath in the user's preferred application (linux, freebsd)."""
+    cmd = ['xdg-open', filepath]
+    log.debug('view: %r', cmd)
+    kwargs = {'stderr': subprocess.DEVNULL} if quiet else {}
+    subprocess.Popen(cmd, **kwargs)
+
+
+@tools.attach(view, 'windows')
+def view_windows(filepath, *, quiet: bool) -> None:
+    """Start filepath with its associated application (windows)."""
+    # TODO: implement quiet=True
+    filepath = os.path.normpath(filepath)
+    log.debug('view: %r', filepath)
+    os.startfile(filepath)  # pytype: disable=module-attr
