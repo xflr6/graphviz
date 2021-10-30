64a8909d0d0e58cb7545980105b053284dfdf361
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-10-30 23:08:30 +0200

    update dependencies

--- graphviz/jupyter_integration.py
+++ graphviz/jupyter_integration.py
@@ -1,11 +1,12 @@
 """Display rendered graph as SVG in Jupyter Notebooks and QtConsole."""
 
+from . import encoding
 from . import rendering
 
 __all__ = ['JupyterSvgIntegration']
 
 
-class JupyterSvgIntegration(rendering.Pipe):
+class JupyterSvgIntegration(rendering.Pipe, encoding.Encoding):
 
     def _repr_svg_(self):
         return self.pipe(format='svg', encoding=self._encoding)
