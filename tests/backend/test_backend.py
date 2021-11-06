9d15b257092905dada7207f9f25c0168f252d391
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-11-06 10:44:24 +0100

    move out unflattening tests

--- tests/backend/test_backend.py
+++ tests/backend/test_backend.py
@@ -1,19 +1,14 @@
 import pathlib
-import re
 import subprocess
 
 import pytest
 
 from graphviz.backend import (render, pipe, unflatten, version, view,
-                              ExecutableNotFound, RequiredArgumentError)
+                              ExecutableNotFound)
 
 import _utils
 
 DOT_BINARY = pathlib.Path('dot')
-
-UNFLATTEN_BINARY = pathlib.Path('unflatten')
-
-SVG_PATTERN = r'(?s)^<\?xml .+</svg>\s*$'
 
 
 @pytest.mark.usefixtures('empty_path')
@@ -26,41 +21,6 @@ SVG_PATTERN = r'(?s)^<\?xml .+</svg>\s*$'
 def test_missing_executable(func, args):
     with pytest.raises(ExecutableNotFound, match=r'execute'):
         func(*args)
-
-
-@pytest.mark.exe
-@pytest.mark.parametrize('source, kwargs, expected', [
-    ('digraph {1 -> 2; 1 -> 3; 1 -> 4}',
-     {'stagger': 3, 'fanout': True, 'chain': 42},
-     'digraph { 1 -> 2 [minlen=1]; 1 -> 3 [minlen=2]; 1 -> 4 [minlen=3]; }'),
-])
-def test_unflatten(source, kwargs, expected):
-    result = unflatten(source, **kwargs)
-    normalized = re.sub(r'\s+', ' ', result.strip())
-    assert normalized == expected
-
-
-def test_unflatten_mocked(capsys, mocker, run):
-    run.return_value = subprocess.CompletedProcess(mocker.sentinel.cmd,
-                                                   returncode=0,
-                                                   stdout='nonresult',
-                                                   stderr='')
-
-    assert unflatten('nonsource') == 'nonresult'
-
-    run.assert_called_once_with([UNFLATTEN_BINARY],
-                                input='nonsource',
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                startupinfo=mocker.ANY,
-                                encoding='utf-8')
-    _utils.check_startupinfo(run.call_args.kwargs['startupinfo'])
-    assert capsys.readouterr() == ('', '')
-
-
-def test_unflatten_stagger_missing():
-    with pytest.raises(RequiredArgumentError, match=r'without stagger'):
-        unflatten('graph {}', fanout=True)
 
 
 @pytest.mark.exe(xfail=True, raises=ExecutableNotFound)
---
+++ tests/backend/test_unflattening.py
@@ -0,0 +1,46 @@
+import pathlib
+import re
+import subprocess
+
+import pytest
+
+import graphviz
+
+import _utils
+
+UNFLATTEN_BINARY = pathlib.Path('unflatten')
+
+
+@pytest.mark.exe
+@pytest.mark.parametrize('source, kwargs, expected', [
+    ('digraph {1 -> 2; 1 -> 3; 1 -> 4}',
+     {'stagger': 3, 'fanout': True, 'chain': 42},
+     'digraph { 1 -> 2 [minlen=1]; 1 -> 3 [minlen=2]; 1 -> 4 [minlen=3]; }'),
+])
+def test_unflatten(source, kwargs, expected):
+    result = graphviz.unflatten(source, **kwargs)
+    normalized = re.sub(r'\s+', ' ', result.strip())
+    assert normalized == expected
+
+
+def test_unflatten_mocked(capsys, mocker, run):
+    run.return_value = subprocess.CompletedProcess(mocker.sentinel.cmd,
+                                                   returncode=0,
+                                                   stdout='nonresult',
+                                                   stderr='')
+
+    assert graphviz.unflatten('nonsource') == 'nonresult'
+
+    run.assert_called_once_with([UNFLATTEN_BINARY],
+                                input='nonsource',
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                startupinfo=mocker.ANY,
+                                encoding='utf-8')
+    _utils.check_startupinfo(run.call_args.kwargs['startupinfo'])
+    assert capsys.readouterr() == ('', '')
+
+
+def test_unflatten_stagger_missing():
+    with pytest.raises(graphviz.RequiredArgumentError, match=r'without stagger'):
+        graphviz.unflatten('graph {}', fanout=True)
