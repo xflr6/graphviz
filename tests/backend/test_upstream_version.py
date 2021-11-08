c685e4879fb229f82bb244a6a319380b42dc4429
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-11-08 20:27:09 +0100

    use invalid cmd

--- tests/backend/test_upstream_version.py
+++ tests/backend/test_upstream_version.py
@@ -22,8 +22,8 @@ def test_version(capsys):
      ('dot - graphviz version 2.44.2~dev.20200927.0217 (20200927.0217)\n', (2, 44, 2)),
      ('dot - graphviz version 2.44.1 (mocked)\n', (2, 44, 1)),
      ('dot - graphviz version 2.44.2~dev.20200704.1652 (mocked)\n', (2, 44, 2))])
-def test_version_mocked(sentinel, mock_run, stdout, expected):
-    mock_run.return_value = subprocess.CompletedProcess(sentinel.cmd,
+def test_version_mocked(mock_run, stdout, expected):
+    mock_run.return_value = subprocess.CompletedProcess(_common.INVALID_CMD,
                                                         returncode=0,
                                                         stdout=stdout,
                                                         stderr=None)
@@ -37,8 +37,8 @@ def test_version_mocked(sentinel, mock_run, stdout, expected):
                                      encoding='ascii')
 
 
-def test_version_parsefail_mocked(sentinel, mock_run):
-    mock_run.return_value = subprocess.CompletedProcess(sentinel.cmd,
+def test_version_parsefail_mocked(mock_run):
+    mock_run.return_value = subprocess.CompletedProcess(_common.INVALID_CMD,
                                                         returncode=0,
                                                         stdout='nonversioninfo',
                                                         stderr=None)
