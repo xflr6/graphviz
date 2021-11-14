967f7e708f6c3723849500c0b08094a8086ff92d
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-11-14 12:49:19 +0100

    skip viewing and add try-example.py exit status
    - also: add --cov-append to skip-exe, improve captions

--- try-examples.py
+++ try-examples.py
@@ -5,20 +5,44 @@
 
 import os
 import pathlib
+import sys
+import unittest.mock
 import warnings
 
 import graphviz  # noqa: F401
 
 EXAMPLES = pathlib.Path('examples')
 
-#graphviz.set_default_format('svg')
+IO_KWARGS = {'encoding': 'utf-8'}
+
+DEFAULT_FORMAT = 'pdf'
+
 
 os.chdir(EXAMPLES)
 
-for path in pathlib.Path().glob('*.py'):
-    print(path)
-    code = path.read_text(encoding='utf-8')
-    try:
-        exec(code)
-    except Exception as e:
-        warnings.warn(e)
+graphviz.set_default_format(DEFAULT_FORMAT)
+
+raised = []
+
+with unittest.mock.patch.object(graphviz.graphs.BaseGraph, '_view') as mock_view:
+    for path in pathlib.Path().glob('*.py'):
+        print(path)
+        code = path.read_text(**IO_KWARGS)
+        try:
+            exec(code)
+        except Exception as e:
+            raised.append(e)
+            warnings.warn(e)
+        else:
+            rendered = f'{path.stem}.gv.{DEFAULT_FORMAT}'
+            assert pathlib.Path(rendered).stat().st_size, f'non-empty {rendered}'
+            mock_view.assert_called_once_with(rendered, DEFAULT_FORMAT, False)
+            mock_view.reset_mock()
+
+if not raised:
+    print('all examples passed without raising (OK)')
+    sys.exit(None)
+else:
+    message = f'{len(raised)} examples raised (WARNING)'
+    print(message, *raised, sep='\n')
+    sys.exit(message)
