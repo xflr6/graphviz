a6a7ae5d89806f3fdebd32b1792707d37bb6cf25
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-11-27 00:13:22 +0100

    add assertions

--- lint-code.py
+++ lint-code.py
@@ -17,8 +17,10 @@ print(f'subprocess.run({cmd!r})')
 try:
     proc = subprocess.run(cmd, check=True)
 except subprocess.CalledProcessError as e:
+    assert e.returncode != 0, f'non-zero returncode: {e}'
     print('FAIL:', e)
     sys.exit(e.returncode)
 else:
+    assert proc.returncode == 0, f'passed: {proc}'
     print('PASS:', proc)
     sys.exit(proc.returncode)
