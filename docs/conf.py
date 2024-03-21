280fa10f3c77737fb9a24f17322f845664555d2a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2024-03-21 08:49:00 +0100

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2013-2024, Sebastian Bank'
 author = 'Sebastian Bank'
 
 # The full version, including alpha/beta/rc tags
-release = '0.20.3'
+release = '0.20.4.dev0'
 
 
 # -- General configuration ---------------------------------------------------
