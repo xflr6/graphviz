83cce3aed72ccd5bd1c2f91c8ec391cf519d7760
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2025-05-30 20:47:21 +0200

    remove deprecated license classifier
    - https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license
    - https://peps.python.org/pep-0639/

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
