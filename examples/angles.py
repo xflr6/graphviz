34eef52f974ae644124f4fedb615809da74be4d3
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2025-09-29 17:28:48 +0200

    use zip(..., strict=True)
    - https://peps.python.org/pep-0618/

--- examples/angles.py
+++ examples/angles.py
@@ -12,7 +12,7 @@ with g.subgraph(name='cluster_1') as c:
     c.attr('node', shape='circle', style='filled', fillcolor='white:black',
            gradientangle='360', label='n9:360', fontcolor='black')
     c.node('n9')
-    for i, a in zip(range(8, 0, -1), range(360 - 45, -1, -45)):
+    for i, a in zip(range(8, 0, -1), range(360 - 45, -1, -45), strict=True):
         c.attr('node', gradientangle=f'{a:d}', label=f'n{i:d}:{a:d}')
         c.node(f'n{i:d}')
     c.attr(label='Linear Angle Variations (white to black gradient)')
@@ -22,7 +22,7 @@ with g.subgraph(name='cluster_2') as c:
     c.attr('node', shape='circle', style='radial', fillcolor='white:black',
            gradientangle='360', label='n18:360', fontcolor='black')
     c.node('n18')
-    for i, a in zip(range(17, 9, -1), range(360 - 45, -1, -45)):
+    for i, a in zip(range(17, 9, -1), range(360 - 45, -1, -45), strict=True):
         c.attr('node', gradientangle=f'{a:d}', label=f'n{i:d}:{a:d}')
         c.node(f'n{i:d}')
     c.attr(label='Radial Angle Variations (white to black gradient)')
